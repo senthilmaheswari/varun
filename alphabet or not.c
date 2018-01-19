9f71dd23dfe9b5703031c80d480967974589d9bc
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-19 16:46:56 +0530

    Create alphabet or not.c

---
+++ alphabet or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char i;
+printf("enter the no:");
+scanf("%d",i);
+if(i>=i&&i<=z)
+printf("given no is alphabet");
+else
+printf("given no is not an alphabet");
+}
