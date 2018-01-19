c86871dfd3060d8f9f747a6e66b69ef319300417
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-19 16:39:22 +0530

    Create n,k integers.c

---
+++ n,k integers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,i;
+sum=0;
+scanf("%d%d",&n,&k);
+printf("%d%d,n,k);
+for(i=0;i<=n;i++)
+printf("%d",a[i]);
+for(i=1;i<=k;i+=)
+sum=sum+a[i];
+printf('%d",sum);
+}
