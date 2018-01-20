f41cb8320f9ee3bc25014d38a30c825fd754a764
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-20 19:27:51 +0530

    Create Even number between range.c

---
+++ Even number between range.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream.h>
+int main ()
+{
+int n, n1, rem,i;
+printf("first number for given range:\n");
+scanf("%d",&n);
+printf("final number for given range:\n");
+scanf("%d",&n1);
+printf("\n even numberbetween %d &%d are:");
+for(i=f1;i<=f2;++i)
+rem=i/2;
+printf  ("%d",rem);
+}
