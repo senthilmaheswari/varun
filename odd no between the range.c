7884f2b62062aeacdaccab72e506d7f5533ce693
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-21 09:43:42 +0530

    Create odd no between the range.c

---
+++ odd no between the range.c
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+int i,num;
+printf("%d",enter the first no range \n:);
+scanf("%d",&n1);
+printf("%d",enter the second the range \n:);
+scanf("%d",n2);
+for(num=n12;num<=n2;num++)
+if(num%2==1)
+printf("%d", enter the range number :\n);
+}
