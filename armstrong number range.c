951cac7defa96f6bcbfe769d445519459492db19
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-21 09:55:07 +0530

    Create armstrong number range.c

---
+++ armstrong number range.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+int low,high,i,temp1,temp2;
+printf("enter the two intervals:\n");
+scanf("%d%d",&low,&high);
+printf("armstrong number between the range:"\n);
+for(i=low+1;i<high;i++)
+temp1!=10;
+++n;
+}
