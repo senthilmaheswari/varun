4ee95bd913e54bd10d9deee9bddd7c99eb45c300
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-21 10:19:46 +0530

    Create arithemetic progression.c

---
+++ arithemetic progression.c
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+int n1,n2,n3,i,s;
+printf("%d",first term of the given arithemetic progreesion:\n");
+scanf("%d",&n1);
+printf("%d",second term of the given arithemetic progression:\n");
+scanf("%d",&n2);
+printf("%d" last number of the given arithemetic progression:\n);
+scanf("%d",&n3);
+for(i=n1+1;i<=n2;i++)
+s=n1/2[2a+(n2-1)d];
+last term=a+(n-1)d;
+printf('%d",arithemetic progression is:\n");
+}
