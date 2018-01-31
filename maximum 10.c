07c6443df0f9996730c6e79ca872a97151282f48
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-01-31 15:54:02 +0530

    Create maximum 10.c

---
+++ maximum 10.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+void main()
+{
+int a[10],i,greatest;
+printf("\n enter the elements:");
+for(i=0;i<=9;i++)
+{
+scanf("%d",&a[i]);
+for(i=0;i<=9;i++)
+{
+for(j=i+1;j<=9;j++)
+{
+if(a[i]<greatest)
+greatest=a[i];
+printf("\n enter the maximum elements:");
+}
+}}
+}
