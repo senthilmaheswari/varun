9ddf5ee12afd74ebe7d42f677607bb970842626d
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-02 16:12:33 +0530

    Create numeric string.c

---
+++ numeric string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a;
+scanf("%s",a);
+if(isdigit(a))
+{
+printf("\n given  number is numeric string");
+}
+else
+{
+printf("\n given number is not an numeric string");
+}
+}
