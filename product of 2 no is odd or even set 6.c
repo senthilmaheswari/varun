a4133101c978cfa888c2b2ae4693dfd0ff944b24
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-12 16:41:08 +0530

    Create product of 2 no is odd or even set 6.c

---
+++ product of 2 no is odd or even set 6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b, c,d;
+printf("\n enter the number:");
+scanf("%d",&a);
+printf("\n enter the number:");
+scanf("%d",&b);
+d=a*b;
+if(d%2==0)
+{
+    printf("\n product no is even");
+}
+else
+{
+    printf("\n product no is odd");
+    
+}
+}
