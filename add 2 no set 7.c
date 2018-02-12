5004e64a104ef860aed64467809aa83fdb21eb10
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-12 16:58:25 +0530

    Create add 2 no set 7.c

---
+++ add 2 no set 7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    printf("\n enter the number:");
+    scanf("%d",&a);
+    printf("\n enter the number:");
+    scanf("%d",&b);
+    c=a+b;
+    if(c%2==0)
+    {
+        printf("\n c no is even");
+        
+    }
+    else
+    {
+        printf("\n c no is odd");
+        
+    }
+    }
