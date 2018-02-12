19852a79390bc440bff5ce0268383035f2c188cf
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-12 16:31:59 +0530

    Update maximum among 10.c

--- maximum among 10.c
+++ maximum among 10.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 void main()
 {
-int a[10],i,j;
+int a[10],i,y;
 printf("\n enter the Nelements:");
 for(i=0;i<=9;i++)
 {
 scanf("%d",&a[i]);
 for(i=0;i<=9;i++)
 {
-for(i=0;i <=9;i++)
+for(y=i+1;y<=9;y++)
 {
-if(a[i]<a[j])
+if(a[i]<a[y])
 {
-a[i]=a[j];
-a[j]=temp;
+a[i]=a[y];
+a[y]=temp;
 printf("enter the maximum number :");
+  printf("%d",a[y]);
 }
 
 
