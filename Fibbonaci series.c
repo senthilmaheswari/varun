2c69bf2301962a344bd4037f3ad5b8ec01d2399f
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-12 16:28:16 +0530

    Update Fibbonaci series.c

--- Fibbonaci series.c
+++ Fibbonaci series.c
@@ -2,21 +2,21 @@
 #include<conio.h>
 void main ()
 {
-int a,b, c;
+int x,y,z;
 clscr();
 printf(" \n enter the fibbonaci series  ");
 scanf("%d",&n);
 }
 int fib(int n)
 {
-int a,b, c,i;
-a=0,b=1;
+int x,y,z,i;
+x=0,y=1;
 printf("\n fibbonaci series \n");
 for(i=0;i<n;i++)
 {
-c=a+b;
-a=b;
-b=c;
-printf("\t %d",c);
+z=x+y;
+x=y;
+y=z;
+printf("\t %d",z);
 }
 }
