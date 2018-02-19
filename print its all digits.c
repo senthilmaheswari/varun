abef77584ca8c0530cef973bf760d25d12b11e83
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-19 15:50:25 +0530

    Create print its all digits.c

---
+++ print its all digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10],i,j;
+    printf("\n enter the string:");
+    scanf("%s",&a);
+    j=strlen(a);
+    for(i=0;i<j;i++)
+    {
+        printf(" %c",a[i]);
+        
+    }
+    }
+    
+
