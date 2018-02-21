95add9e6248b528987e47364052c753c5cdda6bc
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-21 16:44:25 +0530

    Create 62 binary no yes .c

---
+++ 62 binary no yes .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[10];
+    int n;
+    printf("\n enter the number:");
+    scanf("%s",a);
+    if(a[n]=='0'||a[n]=='1')
+    {
+        printf("\n given nu8mber is binary number");
+    }
+    else
+    {
+        printf("\n not a number");
+    }
+    }
+    
+
+
