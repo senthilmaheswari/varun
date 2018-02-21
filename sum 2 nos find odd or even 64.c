88c84862d98fe177d7e8d2c2c5a95b2f657afd54
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-21 16:54:51 +0530

    Create sum 2 nos find odd or even 64.c

---
+++ sum 2 nos find odd or even 64.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b,n;
+printf("\n enter the numbers:");
+scanf("%d",&a);
+printf("\n enter the number:");
+scanf("%d",&b);
+n=a+b;
+if(n%2==0)
+{
+    printf("\n givem no is even");
+}
+    else
+    
+    {
+        printf("\n given no is odd");
+    }
+}
