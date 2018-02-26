c71890e53b0770e4a1a1e2b3aafaf3251de855a3
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-26 15:55:24 +0530

    Create prime no66.c

---
+++ prime no66.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,flag=0;
+    printf("\n enter the number:");
+    scanf("%d",&n);
+    for(i=2;i<n/2;i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+        if(flag==0)
+        {
+            printf("\n prime no");
+        }
+        else
+        {
+            printf(":\n not an prime no");
+        }
+    }
+        
+    
+    
