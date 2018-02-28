506180c5f30629f75e6aea8fd7b87641bbc92f3d
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-02-28 16:04:41 +0530

    Create 42 .c

---
+++ 42 .c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int m,n;
+    char a[50],b[50];
+    printf("\n enter the string:");
+    scanf(" %s", a);
+    printf("\n enter the string");
+    scanf(" %s", b);
+    m=strlen(a);
+    n=strlen(b);
+    if(m>n)
+    
+        printf("%s",a);
+    
+        else if(n=m)
+        
+            printf("%s",a),printf("%s",b);
+            
+        
+    
+    else( n>m)
+    
+    
+      ;  printf("%s",b);
+    
+    return 0;
+    
+}
+
+
+
