4345b3860886ed963ab480c681695e19e83e74e3
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-03-02 15:50:41 +0530

    Create concatenate 43.c

---
+++ concatenate 43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[34],b[34];
+printf("\n enter the string:");
+scanf("%s",a);
+printf("\n enter the string:");
+scanf("%s",b);
+printf("%s%s",a,b);
+return 0;
+}
