d2b2a63600c0a4907bb5c5b9330bd39327ae09f3
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-03-12 16:23:35 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char c[51];
+    printf("\n enter the string:");
+    scanf("%s", &c);
+    if((c=a<<z&&a>>z)&&(c=A<<Z&&A>>Z)&&(c='\0'||c='1'||c='2'||c='3'||c='4'||c='5'||c='6'||c='7'||c='8'||c='9')
+    {
+        printf("\n IT CONTAINS BOTH NUMBERS & ALPHABETS");
+        
+    }
+    else
+    {
+        printf("\n it does not contains alphabets and numeric");
+    }
+    return 0;
+}
