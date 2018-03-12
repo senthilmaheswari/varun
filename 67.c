cd4c7d9850f3d8dbf6c9740bb6f549668a92ab28
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-03-12 16:58:23 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int m,k,j;
+	printf("enter the number");
+	scanf("%d",&m);
+	for(j=m;j<k+10;j++)
+	{
+		if(j%10==0)
+		{
+			printf("%d",j);
+		}
+	}
+	return 0;
+}
+
+
