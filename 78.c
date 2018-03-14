eff16d5d33f145cfb0030606faacb3ceb3d57e6b
senthilmaheswari <35249790+senthilmaheswari@users.noreply.github.com>
2018-03-14 16:39:37 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("\n enter the number:");
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("\n it is a multiple of 13");
+    }
+    else
+    {
+        printf("\n it is not multiple of 13 ");
+        
+    }
+    
+
+    return 0;
+}
